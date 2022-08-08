41d27d28439dc6d32315f6e574c8e1491f91b720
Freddy Barragán <freb2@IndTech-Cesar.Ci2.local>
2022-08-08 16:33:06 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='img'>
+        <div className='img-stripe stripe1'> 
+          <img src='https://images.pexels.com/photos/12571192/pexels-photo-12571192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12604653/pexels-photo-12604653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12800830/pexels-photo-12800830.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/4308937/pexels-photo-4308937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12973509/pexels-photo-12973509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/11843489/pexels-photo-11843489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+        </div>
+        <div className='img-stripe stripe2'>
+          <img src='https://images.pexels.com/photos/12571192/pexels-photo-12571192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12604653/pexels-photo-12604653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12800830/pexels-photo-12800830.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/4308937/pexels-photo-4308937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12973509/pexels-photo-12973509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/11843489/pexels-photo-11843489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+        </div>
+        <div className='img-stripe stripe3'>
+          <img src='https://images.pexels.com/photos/12571192/pexels-photo-12571192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12604653/pexels-photo-12604653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12800830/pexels-photo-12800830.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/4308937/pexels-photo-4308937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/12973509/pexels-photo-12973509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+          <img src='https://images.pexels.com/photos/11843489/pexels-photo-11843489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='test' />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const dataNft = [
+    {
+        id: 1,
+        title: 'NFT 1',
+        price: '$100',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/13010671/pexels-photo-13010671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        id: 2,
+        title: 'NFT 2',
+        price: '$150',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/10049163/pexels-photo-10049163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        id: 3,
+        title: 'NFT 3',
+        price: '$200',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/5381500/pexels-photo-5381500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        id: 4,
+        title: 'NFT 4',
+        price: '$250',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/5720783/pexels-photo-5720783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        id: 5,
+        title: 'NFT 5',
+        price: '$300',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/12891215/pexels-photo-12891215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        id: 6,
+        title: 'NFT 6',
+        price: '$350',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/4224255/pexels-photo-4224255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        id: 7,
+        title: 'NFT 7',
+        price: '$400',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/11540260/pexels-photo-11540260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        id: 8,
+        title: 'NFT 8',
+        price: '$450',
+        category: 'NFT Category',
+        linkImg: 'https://images.pexels.com/photos/12144473/pexels-photo-12144473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+]
